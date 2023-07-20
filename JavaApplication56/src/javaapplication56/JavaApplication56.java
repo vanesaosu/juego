669d9e19f02bdd0039c40ee7f0d2e78c6929ef46
Mona Lisa <vanesa10osuna@gmail.com>
2023-07-19 23:21:14 -0300

    agregar

---
+++ JavaApplication56/src/javaapplication56/JavaApplication56.java
@@ -0,0 +1,16 @@
+
+package javaapplication56;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class JavaApplication56 {
+
+    
+    public static void main(String[] args) {
+        
+        List<videoJuego>listaVideoJuegos= new ArrayList<videoJuego>();
+    }
+    
+}
